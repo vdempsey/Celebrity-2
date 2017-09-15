9ca12bf9c1dcced18f3186f56185807d97f425bf
Valia Dempsey <valentinaml@gmail.com>
2017-09-14 22:22:23 -0700

    Edit index.html, entered js code for elon, correct display none from img to id

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
-  var elonInput1 = $parseInt("input#gridRadios1").val();
-  var elonInput2 = $parseInt("input#gridRadios5").val();
-  var elonInput3 = $parseInt("input#gridRadios9").val();
+  $("form").submit(function(event) {
+    var elonInput1 = document.getElementById("gridRadios1");
+    var elonInput2 = document.getElementById("gridRadios5");
+    var elonInput3 = document.getElementById("gridRadios9");
 
-  var inputTotal = elonInput1 + elonInput2 + elonInput3;
-$(#submit)
-  if (inputTotal === 9) {
-    //$('#Elon').show();
-    alert(inputTotal);
-  //} else if (age === 18) {
-  //  alert("Welcome NEW voter!");
-  //  $('#adult').show();
-  //} else {
-  //  $('#minor').show();
-  }
+    if ((elonInput1).checked && (elonInput2).checked && (elonInput3).checked) {
+      $('#elon').show();
+    }
+
+    event.preventDefault();
+
+  });
 });
